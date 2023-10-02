669c72a4731bf348d1fd6200729a8a873e47776b
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-10-02 17:54:06 +0300

    update

--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@
 // Include Composer's autoloader to load dependencies
 require_once '../vendor/autoload.php';
 
-// Define the project's root directory
 define('PROJECT_ROOT', dirname(__DIR__));
-
 require_once '../src/session.php';
 require_once '../src/routes.php';
